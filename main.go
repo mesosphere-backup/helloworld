5e9bd70ac41af4d2cac0394360881d8db7d6a5e0
Sargun Dhillon <sargun@sargun.me>
2015-12-10 01:10:30 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "os"
+  "io"
+  "net/http"
+)
+
+var listener string
+
+func init() {
+  flag.StringVar(&listener, "listener", ":1234", "Listener address")
+}
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+  hostname, _ := os.Hostname()
+  pid := os.Getpid()
+  ret := fmt.Sprintf("Hello world (%d) app running at: %s, on hostname: %s\n", pid, listener, hostname)
+  io.WriteString(w, ret)
+}
+
+func main() {
+  fmt.Println("Hello world server starting up")
+  http.HandleFunc("/", HelloServer)
+  http.ListenAndServe(listener, nil)
+}
